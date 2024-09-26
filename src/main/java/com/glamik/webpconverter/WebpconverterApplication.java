41ce1516b50196a4803e2c9837097a34cad18092
marl0vv <23glad02@mail.ru>
2024-09-27 07:59:15 +1000

    Initial commit

---
+++ src/main/java/com/glamik/webpconverter/WebpconverterApplication.java
@@ -0,0 +1,13 @@
+package com.glamik.webpconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glamik/webpconverter/WebpconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glamik.webpconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
