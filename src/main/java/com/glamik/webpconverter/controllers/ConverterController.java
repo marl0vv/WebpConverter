7785c030ea87d0ffab51854ebdea205a44dcea60
marl0vv <23glad02@mail.ru>
2024-10-01 13:31:49 +1000

    Add method to add conversion request in database
    Add convertImageAsync that adds conversion task to database

--- src/main/java/com/glamik/webpconverter/controllers/ConverterController.java
+++ src/main/java/com/glamik/webpconverter/controllers/ConverterController.java
@@ -1,7 +1,10 @@
 package com.glamik.webpconverter.controllers;
 
+import com.glamik.webpconverter.model.ConversionTask;
+import com.glamik.webpconverter.repository.ConversionTaskRepository;
 import com.glamik.webpconverter.service.ConverterService;
 
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.PathResource;
 import org.springframework.http.HttpHeaders;
@@ -14,12 +17,15 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+//@AllArgsConstructor
 public class ConverterController {
 
     private final ConverterService converterService;
+    private final ConversionTaskRepository conversionTaskRepository;
 
     @PostMapping("/convert-to-webp")
     public ResponseEntity<PathResource> convertImage(@RequestParam("image") MultipartFile imageFile) {
@@ -54,6 +60,26 @@ public class ConverterController {
             deleteIfExists(tempInputFile);
         }
     }
+
+    @PostMapping("/convert-to-webp/async")
+    public ResponseEntity<UUID> convertImageAsync(@RequestParam("image") MultipartFile imageFile) {
+        File tempInputFile = null;
+        String originalFilename;
+        try {
+            originalFilename = imageFile.getOriginalFilename();
+            tempInputFile = File.createTempFile("input-", getFileExtension(originalFilename));
+            imageFile.transferTo(tempInputFile);
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+
+        ConversionTask conversionTask = conversionTaskRepository
+                .save(new ConversionTask("PROCESSING", originalFilename));
+        return ResponseEntity.ok()
+                .body(conversionTask.getId());
+    }
+
 
     private String getFileExtension(String filename) {
         int lastDot = filename.lastIndexOf('.');
--- src/main/java/com/glamik/webpconverter/model/ConversionTask.java
+++ src/main/java/com/glamik/webpconverter/model/ConversionTask.java
@@ -1,7 +1,9 @@
 package com.glamik.webpconverter.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.util.UUID;
@@ -9,6 +11,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "conversion_tasks")
 @Getter @Setter
+@RequiredArgsConstructor
 public class ConversionTask {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -19,4 +22,9 @@ public class ConversionTask {
 
     @Column(name = "file_name", nullable = false)
     private String fileName;
+
+    public ConversionTask(String taskStatus, String fileName) {
+        this.taskStatus = taskStatus;
+        this.fileName = fileName;
+    }
 }
