aced8df361c72ecae664c83935806dc8c222acf3
marl0vv <23glad02@mail.ru>
2024-10-04 17:25:00 +1000

    Add deletion of old converted images after some time
    Add deletion of old converted images after time that is contained in application.properties

--- src/main/java/com/glamik/webpconverter/scheduler/ConversionTaskScheduler.java
+++ src/main/java/com/glamik/webpconverter/scheduler/ConversionTaskScheduler.java
@@ -5,10 +5,15 @@ import com.glamik.webpconverter.enums.ConversionTaskStatus;
 import com.glamik.webpconverter.model.ConversionTask;
 import com.glamik.webpconverter.repository.ConversionTaskRepository;
 
+import com.glamik.webpconverter.service.FileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -17,6 +22,10 @@ public class ConversionTaskScheduler {
 
     private final ConversionTaskRepository conversionTaskRepository;
     private final ProcessConversionTaskCommand processConversionTaskCommand;
+    private final FileService fileService;
+
+    @Value("${deletion.time.millis}")
+    private long deletionTimeMillis;
 
     @Scheduled(fixedDelay = 10000)
     public void processPendingConversionTasks() {
@@ -26,4 +35,17 @@ public class ConversionTaskScheduler {
         }
     }
 
+
+    @Scheduled(initialDelayString = "${deletion.time.millis}", fixedDelayString = "${deletion.time.millis}")
+    public void deleteOldConvertedImages() {
+        List<ConversionTask> pendingTasks = conversionTaskRepository.findByStatusOrderByTaskCreationDate(ConversionTaskStatus.SUCCESS);
+
+        for (ConversionTask task : pendingTasks) {
+            long timeBetween = Duration.between(task.getTaskCreationDate(), LocalDateTime.now()).toMillis();
+            if (timeBetween >= deletionTimeMillis) {
+                File convertedFile = fileService.getOutputFile(task.getConvertedName());
+                convertedFile.delete();
+            }
+        }
+    }
 }
