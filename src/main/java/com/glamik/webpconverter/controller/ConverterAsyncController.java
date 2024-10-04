da35d700139ad97d12adaa3d2e3fd2dbbd7944b9
marl0vv <23glad02@mail.ru>
2024-10-04 16:15:19 +1000

    Add GET request for converted image
    Add GET request that returns a converted image or 404 if image is not ready of there was an error in conversion

--- src/main/java/com/glamik/webpconverter/controller/ConverterAsyncController.java
+++ src/main/java/com/glamik/webpconverter/controller/ConverterAsyncController.java
@@ -6,14 +6,18 @@ import com.glamik.webpconverter.enums.ConversionTaskStatus;
 import com.glamik.webpconverter.enums.ErrorMessage;
 import com.glamik.webpconverter.model.ConversionTask;
 
-import com.glamik.webpconverter.repository.ConversionTaskRepository;
 import com.glamik.webpconverter.service.ConversionTaskService;
+import com.glamik.webpconverter.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.PathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.UUID;
 
 @RestController
@@ -22,6 +26,7 @@ public class ConverterAsyncController {
 
     private final SaveConversionTaskCommand saveConversionTaskCommand;
     private final ConversionTaskService conversionTaskService;
+    private final FileService fileService;
 
     @PostMapping("/convert-to-webp/async")
     public ResponseEntity<UUID> convertImageAsync(@RequestParam("image") MultipartFile imageFile) {
@@ -31,21 +36,34 @@ public class ConverterAsyncController {
     }
 
     @GetMapping("/convert-to-webp/async/{taskId}/status")
-    public ResponseEntity<ConversionTaskStatusResponse> getTaskStatus(@PathVariable("taskId") UUID taskId) {
+    public ResponseEntity<ConversionTaskStatusResponse> getTaskStatus(@PathVariable UUID taskId) {
         ConversionTask conversionTask = conversionTaskService.getConversionTask(taskId);
         ConversionTaskStatus status = conversionTask.getStatus();
 
-        if (status == ConversionTaskStatus.ERROR) {
-            ErrorMessage errorMessage = conversionTask.getErrorMessage();
-            ConversionTaskStatusResponse response = new ConversionTaskStatusResponse(status, errorMessage);
-            return ResponseEntity.ok(response);
-        } else {
-            ConversionTaskStatusResponse response = ConversionTaskStatusResponse.builder()
-                    .status(status)
-                    .build();
-            return ResponseEntity.ok(response);
-        }
+        ConversionTaskStatusResponse response = ConversionTaskStatusResponse.builder()
+                .status(status)
+                .errorMessage(status == ConversionTaskStatus.ERROR ? conversionTask.getErrorMessage() : null)
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 
-    
+    @GetMapping("/convert-to-webp/async/{taskId}")
+    public ResponseEntity<PathResource> getConvertedImage(@PathVariable UUID taskId) {
+        ConversionTask conversionTask = conversionTaskService.getConversionTask(taskId);
+
+        if (conversionTask.getStatus() != ConversionTaskStatus.SUCCESS) {
+            return ResponseEntity.notFound().build();
+        }
+
+        File outputFile = fileService.getOutputFile(conversionTask.getConvertedName());
+        PathResource resource = new PathResource(outputFile.toPath());
+
+        String outputFileName = conversionTask.getOriginalName() + ".webp";
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + outputFileName + "\"")
+                .contentType(MediaType.asMediaType(MimeType.valueOf("image/webp")))
+                .body(resource);
+    }
+
 }
