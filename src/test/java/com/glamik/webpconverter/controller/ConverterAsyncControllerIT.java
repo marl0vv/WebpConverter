1846f8a20f3b197e8f2d6da06381995ff3223072
marl0vv <23glad02@mail.ru>
2024-10-10 11:28:50 +1000

    Add tests for second request

--- src/test/java/com/glamik/webpconverter/controller/ConverterAsyncControllerIT.java
+++ src/test/java/com/glamik/webpconverter/controller/ConverterAsyncControllerIT.java
@@ -1,7 +1,11 @@
 package com.glamik.webpconverter.controller;
 
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.junit5.api.DBRider;
 import com.glamik.webpconverter.BaseSpringBootApplicationTest;
 import com.glamik.webpconverter.enums.ConversionTaskStatus;
+import com.glamik.webpconverter.enums.ErrorMessage;
 import com.glamik.webpconverter.model.ConversionTask;
 import com.glamik.webpconverter.repository.ConversionTaskRepository;
 import com.glamik.webpconverter.service.FileService;
@@ -18,13 +22,14 @@ import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.matchesPattern;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@DBRider
+@DBUnit(caseSensitiveTableNames = true)
 class ConverterAsyncControllerIT extends BaseSpringBootApplicationTest {
 
-    private static final String REQUEST_URL = "/convert-to-webp/async";
+    private static final String CONVERT_TO_WEBP_ASYNC_URL = "/convert-to-webp/async";
     private static final String IMAGE_FIELD_NAME = "image";
     private static final String TEST_IMAGE_NAME = "test-image.jpg";
     private static final String IMAGE_MEDIA_TYPE = MediaType.IMAGE_JPEG_VALUE;
@@ -49,7 +54,7 @@ class ConverterAsyncControllerIT extends BaseSpringBootApplicationTest {
     }
 
     private UUID conversionTaskRequest(MockMultipartFile mockMultipartFile) throws Exception {
-        String responseContent = mockMvc.perform(multipart(REQUEST_URL)
+        String responseContent = mockMvc.perform(multipart(CONVERT_TO_WEBP_ASYNC_URL)
                         .file(mockMultipartFile))
                 .andExpect(status().isOk())
                 .andExpect(content().string(matchesPattern(UUID_PATTERN)))
@@ -89,4 +94,38 @@ class ConverterAsyncControllerIT extends BaseSpringBootApplicationTest {
         assertThatFileCreatedOnDisc(task.getFilesystemName());
     }
 
+
+    private void getStatusRequest(UUID exampleId, ConversionTaskStatus expectedStatus, ErrorMessage expectedErrorMessage) throws Exception {
+        var resultActions = mockMvc.perform(get("/convert-to-webp/async/{taskId}/status", exampleId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.status").value(expectedStatus.toString()));
+
+        if (expectedErrorMessage != null) {
+            resultActions.andExpect(jsonPath("$.errorMessage").value(expectedErrorMessage.toString()));
+        } else {
+            resultActions.andExpect(jsonPath("$.errorMessage").isEmpty());
+        }
+    }
+
+    @Test
+    @DataSet("example-data-single-success.yml")
+    void getConversionTaskStatusSuccess() throws Exception {
+        UUID exampleId = UUID.fromString("607c09c6-3032-4711-a018-118d8f709c8c");
+        getStatusRequest(exampleId, ConversionTaskStatus.SUCCESS, null);
+    }
+
+    @Test
+    @DataSet("example-data-single-error-not-image.yml")
+    void getConversionTaskStatusErrorNotAnImage() throws Exception {
+        UUID exampleId = UUID.fromString("718091d1-70c6-43df-b8ce-fb0eaf6fcf30");
+        getStatusRequest(exampleId, ConversionTaskStatus.ERROR, ErrorMessage.INPUT_FILE_IS_NOT_AN_IMAGE);
+    }
+
+    @Test
+    @DataSet("example-data-single-error-null.yml")
+    void getConversionTaskStatusErrorNullOrCorrupted() throws Exception {
+        UUID exampleId = UUID.fromString("718091d1-70c6-43df-b8ce-fb0eaf6fcf30");
+        getStatusRequest(exampleId, ConversionTaskStatus.ERROR, ErrorMessage.INPUT_FILE_IS_NULL_OR_CORRUPTED);
+    }
 }
